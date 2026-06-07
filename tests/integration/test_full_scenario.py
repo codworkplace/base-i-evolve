dfd439a8b856283d0d5d403968100acc89a539b3
codeworkplace <workkm30@gmail.com>
2026-06-07 17:21:26 +0500

    Update integration test with authentication

--- tests/integration/test_full_scenario.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import text
-import uuid
-
-
-def test_full_user_journey(client: TestClient, db_session):
-    # ... (код из моего последнего сообщения)
